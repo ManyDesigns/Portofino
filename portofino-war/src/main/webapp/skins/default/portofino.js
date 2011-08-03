3cdcbba3dd0b185737a74072935382fb73ea5f10
Paolo Predonzani <paolo.predonzani@manydesigns.com>
2011-08-03 11:09:04 +0200

    tuning grafico drag-n-drop
    --HG--
    branch : stripes

--- portofino-war/src/main/webapp/skins/default/portofino.js
+++ portofino-war/src/main/webapp/skins/default/portofino.js
@@ -25,6 +25,14 @@ $(function() {
 
 function enablePortletDragAndDrop() {
     $("div.portletContainer").sortable({
-        connectWith: "div.portletContainer"
-    }).css('padding', '1em 0').css("border", "1px dashed grey").css("margin-bottom", "1em").css("min-height", "10em");
+        connectWith: "div.portletContainer",
+        placeholder: "sortablePlaceholder",
+        cursor: "move", // cursor image
+        revert: true, // moves the portlet to its new position with a smooth transition
+        tolerance: "pointer" // mouse pointer overlaps the droppable
+    }).disableSelection()
+            .css('padding', '1em 0')
+            .css("border", "1px dashed grey")
+            .css("margin-bottom", "1em")
+            .css("min-height", "12em");
 }
