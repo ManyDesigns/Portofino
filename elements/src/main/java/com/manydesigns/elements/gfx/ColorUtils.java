b90c9ca99ffeeed13ee9ef7089f4f4ed259a9edf
Paolo Predonzani <paolo.predonzani@manydesigns.com>
2012-02-20 17:58:14 +0100

    ColorUtils: calcolo luminance e blend in modo screen.

--- elements/src/main/java/com/manydesigns/elements/gfx/ColorUtils.java
+++ elements/src/main/java/com/manydesigns/elements/gfx/ColorUtils.java
@@ -62,6 +62,23 @@ public class ColorUtils {
         return new Color(components[0], components[1], components[2], components[3]);
     }
 
+    public static Color screen(Color color1, Color color2) {
+        float[] components1 = color1.getRGBComponents(null);
+        float[] components2 = color2.getRGBComponents(null);
+        float[] components = new float[4];
+        for (int i = 0; i < 4; i++) {
+            components[i] = 1.0f - (1.0f - components1[i]) * (1.0f - components2[i]);
+        }
+        return new Color(components[0], components[1], components[2], components[3]);
+    }
+
+    public static float getLuminance(Color color) {
+        float[] components = color.getRGBComponents(null);
+        return 0.3f * components[0] +
+                0.59f * components[1] +
+                0.11f * components[2];
+    }
+
     public static String toHexString(Color color) {
         return String.format("%x", color.getRGB());
     }
