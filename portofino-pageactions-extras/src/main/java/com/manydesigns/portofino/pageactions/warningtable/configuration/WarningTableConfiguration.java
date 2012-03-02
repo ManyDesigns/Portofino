6816aefd0a3a2772714cd45a9dd14fae74a8f2f5
Giampiero Granatella <giampiero.granatella@manydesigns.com>
2012-03-02 14:35:11 +0100

    plugin Warning Table

---
+++ portofino-pageactions-extras/src/main/java/com/manydesigns/portofino/pageactions/warningtable/configuration/WarningTableConfiguration.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2005-2011 ManyDesigns srl.  All rights reserved.
+ * http://www.manydesigns.com/
+ *
+ * Unless you have purchased a commercial license agreement from ManyDesigns srl,
+ * the following license terms apply:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * There are special exceptions to the terms and conditions of the GPL
+ * as it is applied to this software. View the full text of the
+ * exception in file OPEN-SOURCE-LICENSE.txt in the directory of this
+ * software distribution.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; and without the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/gpl.txt
+ * or write to:
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
+ */
+package com.manydesigns.portofino.pageactions.warningtable.configuration;
+
+import com.manydesigns.elements.annotations.LabelI18N;
+import com.manydesigns.elements.annotations.Required;
+import com.manydesigns.portofino.application.Application;
+import com.manydesigns.portofino.dispatcher.PageActionConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Paolo Predonzani     - paolo.predonzani@manydesigns.com
+ * @author Angelo Lupo          - angelo.lupo@manydesigns.com
+ * @author Giampiero Granatella - giampiero.granatella@manydesigns.com
+ * @author Alessio Stalla       - alessio.stalla@manydesigns.com
+ */
+
+@XmlRootElement(name = "configuration")
+@XmlAccessorType(XmlAccessType.NONE)
+public class WarningTableConfiguration implements PageActionConfiguration {
+    public static final String copyright =
+            "Copyright (c) 2005-2011, ManyDesigns srl";
+
+    //**************************************************************************
+    // Fields
+    //**************************************************************************
+    String header1;
+    String header2;
+
+    //**************************************************************************
+    // Logging
+    //**************************************************************************
+
+    public static final Logger logger =
+            LoggerFactory.getLogger(WarningTableConfiguration.class);
+
+    //**************************************************************************
+    // Constructors
+    //**************************************************************************
+
+    public WarningTableConfiguration() {
+        super();
+    }
+
+    //**************************************************************************
+    // Configuration implementation
+    //**************************************************************************
+
+    public void init(Application application) {}
+
+    //**************************************************************************
+    // Getters/setters
+    //**************************************************************************
+
+    @XmlAttribute
+    @Required
+    @LabelI18N("warningTable.configuration.header1")
+    public String getHeader1() {
+        return header1;
+    }
+
+
+    public void setHeader1(String header1) {
+        this.header1 = header1;
+    }
+
+    @XmlAttribute
+    @Required
+    @LabelI18N("warningTable.configuration.header2")
+    public String getHeader2() {
+        return header2;
+    }
+
+    public void setHeader2(String header2) {
+        this.header2 = header2;
+    }
+
+
+}
