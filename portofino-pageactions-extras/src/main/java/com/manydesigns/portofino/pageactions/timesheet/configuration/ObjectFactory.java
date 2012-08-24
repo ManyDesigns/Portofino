760b84bcf0948acb8ba680c1d1f281e921d7d50c
Alessio Stalla <alessio.stalla@manydesigns.com>
2012-08-24 17:18:34 +0200

    #542

--- portofino-pageactions-extras/src/main/java/com/manydesigns/portofino/pageactions/timesheet/configuration/ObjectFactory.java
+++
@@ -1,8 +0,0 @@
-package com.manydesigns.portofino.pageactions.timesheet.configuration;
-
-import javax.xml.bind.annotation.XmlRegistry;
-
-@XmlRegistry
-public class ObjectFactory {
-    public TimesheetConfiguration createTimesheetConfiguration() { return new TimesheetConfiguration(); }
-}
