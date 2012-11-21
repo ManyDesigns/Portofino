49219b0101465eb2ffe7da77fe03a82721f2cbc0
Alessio Stalla <alessio.stalla@manydesigns.com>
2012-11-21 13:45:38 +0100

    #721

--- portofino-web/src/main/resources/com/manydesigns/portofino/pageactions/crud/script_template.groovy
+++ portofino-web/src/main/resources/com/manydesigns/portofino/pageactions/crud/script_template.groovy
@@ -4,13 +4,16 @@ import com.manydesigns.elements.messages.*
 import com.manydesigns.elements.reflection.*
 import com.manydesigns.portofino.*
 import com.manydesigns.portofino.application.*
+import com.manydesigns.portofino.buttons.*
 import com.manydesigns.portofino.buttons.annotations.*
 import com.manydesigns.portofino.dispatcher.*
 import com.manydesigns.portofino.model.database.*
 import com.manydesigns.portofino.pageactions.*
 import com.manydesigns.portofino.security.*
+import com.manydesigns.portofino.shiro.*
 
 import net.sourceforge.stripes.action.*
+import org.apache.shiro.*
 import org.hibernate.*
 import org.hibernate.criterion.*
 
