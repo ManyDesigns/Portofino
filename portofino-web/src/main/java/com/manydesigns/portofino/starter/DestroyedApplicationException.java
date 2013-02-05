f0da8172eb4f3d76e287b293e9d18ff0b619577a
Alessio Stalla <alessio.stalla@manydesigns.com>
2013-02-05 10:34:36 +0100

    #866

--- portofino-web/src/main/java/com/manydesigns/portofino/starter/DestroyedApplicationException.java
+++ portofino-web/src/main/java/com/manydesigns/portofino/starter/DestroyedApplicationException.java
@@ -2,29 +2,20 @@
  * Copyright (C) 2005-2013 ManyDesigns srl.  All rights reserved.
  * http://www.manydesigns.com/
  *
- * Unless you have purchased a commercial license agreement from ManyDesigns srl,
- * the following license terms apply:
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as published by
- * the Free Software Foundation.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * There are special exceptions to the terms and conditions of the GPL
- * as it is applied to this software. View the full text of the
- * exception in file OPEN-SOURCE-LICENSE.txt in the directory of this
- * software distribution.
- *
- * This program is distributed WITHOUT ANY WARRANTY; and without the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see http://www.gnu.org/licenses/gpl.txt
- * or write to:
- * Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330,
- * Boston, MA  02111-1307  USA
- *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
 package com.manydesigns.portofino.starter;
