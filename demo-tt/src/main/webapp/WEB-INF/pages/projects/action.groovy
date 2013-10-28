32042bd40e3ab65e2f6c572dd9672e8f191b5756
Paolo Predonzani <paolo.predonzani@manydesigns.com>
2013-10-28 14:43:43 +0100

    basic structure of new demo-tt (based on portofino-4.1.beta5-SNAPSHOT)

---
+++ demo-tt/src/main/webapp/WEB-INF/pages/projects/action.groovy
@@ -0,0 +1,83 @@
+package com.manydesigns.portofino.pageactions.crud
+
+import com.manydesigns.portofino.security.AccessLevel
+import com.manydesigns.portofino.security.RequiresPermissions
+import com.manydesigns.portofino.security.SupportsPermissions
+import net.sourceforge.stripes.action.RedirectResolution
+import net.sourceforge.stripes.action.Resolution
+
+@SupportsPermissions([ CrudAction.PERMISSION_CREATE, CrudAction.PERMISSION_EDIT, CrudAction.PERMISSION_DELETE ])
+@RequiresPermissions(level = AccessLevel.VIEW)
+class ProjectsCrudAction extends CrudAction {
+
+    //Automatically generated on Mon Oct 28 12:15:50 CET 2013 by ManyDesigns Portofino
+    //Write your code here
+    @Override
+    Resolution read() {
+        def path = getContext().actualServletPath
+        while (path.endsWith("/")) {
+            path = path.substring(0, path.length() -1 );
+        }
+        return new RedirectResolution(path + "/summary");
+    }
+
+    //**************************************************************************
+    // Extension hooks
+    //**************************************************************************
+
+    protected void createSetup(Object object) {
+        object.last_ticket = 0L;
+    }
+
+    protected boolean createValidate(Object object) {
+        return true;
+    }
+
+    protected void createPostProcess(Object object) {}
+
+
+    protected void editSetup(Object object) {}
+
+    protected boolean editValidate(Object object) {
+        return true;
+    }
+
+    protected void editPostProcess(Object object) {}
+
+
+    protected boolean deleteValidate(Object object) {
+        return true;
+    }
+
+    protected void deletePostProcess(Object object) {}
+
+
+    protected Resolution getBulkEditView() {
+        return super.getBulkEditView();
+    }
+
+    protected Resolution getCreateView() {
+        return super.getCreateView();
+    }
+
+    protected Resolution getEditView() {
+        return super.getEditView();
+    }
+
+    protected Resolution getReadView() {
+        return super.getReadView();
+    }
+
+    protected Resolution getSearchView() {
+        return super.getSearchView();
+    }
+
+    protected Resolution getEmbeddedSearchView() {
+        return super.getEmbeddedSearchView();
+    }
+
+    protected Resolution getSearchResultsPageView() {
+        return super.getSearchResultsPageView()
+    }
+
+}
