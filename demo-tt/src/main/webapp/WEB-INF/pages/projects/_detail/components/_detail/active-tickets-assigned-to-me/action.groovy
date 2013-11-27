8d1537dfc8ac52e63f34b1285d3e4eb588a27717
Paolo Predonzani <paolo.predonzani@manydesigns.com>
2013-11-27 14:37:59 +0100

    #1487 #1488
    --HG--
    rename : demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/versions/_detail/active-tickets-assigned-to-me/page.xml => demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/components/_detail/active-tickets-assigned-to-me/page.xml
    rename : demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/versions/_detail/recently-updated-tickets/action.groovy => demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/components/_detail/recently-updated-tickets/action.groovy

---
+++ demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/components/_detail/active-tickets-assigned-to-me/action.groovy
@@ -0,0 +1,45 @@
+import com.manydesigns.elements.ElementsThreadLocals
+import com.manydesigns.portofino.di.Inject
+import com.manydesigns.portofino.modules.DatabaseModule
+import com.manydesigns.portofino.pageactions.custom.CustomAction
+import com.manydesigns.portofino.persistence.Persistence
+import com.manydesigns.portofino.security.AccessLevel
+import com.manydesigns.portofino.security.RequiresPermissions
+import net.sourceforge.stripes.action.DefaultHandler
+import net.sourceforge.stripes.action.ForwardResolution
+import net.sourceforge.stripes.action.Resolution
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.subject.Subject
+import org.hibernate.Session
+import org.hibernate.criterion.Order
+import org.hibernate.criterion.Restrictions
+
+@RequiresPermissions(level = AccessLevel.VIEW)
+class ProjectsActiveTicketsAssignedToMeAction extends CustomAction {
+
+    @Inject(DatabaseModule.PERSISTENCE)
+    private Persistence persistence;
+
+    List tickets;
+
+    @DefaultHandler
+    public Resolution execute() {
+        Object component = ElementsThreadLocals.getOgnlContext().get("component");
+        Session session = persistence.getSession("tt");
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.isAuthenticated()) {
+            tickets = session.createCriteria("tickets")
+                    .add(Restrictions.eq("assignee", subject.getPrincipal().id))
+                    .add(Restrictions.ne("state", 4L))
+                    .addOrder(Order.asc("n"))
+                    .createCriteria("fk_affected_component_ticket")
+                    .add(Restrictions.eq("component", component.id))
+                    .list();
+        } else {
+            tickets = Collections.EMPTY_LIST;
+        }
+
+        return new ForwardResolution("/jsp/common/active-tickets-assigned-to-me.jsp");
+    }
+
+}
---
+++ demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/components/_detail/recently-updated-tickets/action.groovy
@@ -0,0 +1,37 @@
+import com.manydesigns.elements.ElementsThreadLocals
+import com.manydesigns.portofino.di.Inject
+import com.manydesigns.portofino.modules.DatabaseModule
+import com.manydesigns.portofino.pageactions.custom.CustomAction
+import com.manydesigns.portofino.persistence.Persistence
+import com.manydesigns.portofino.security.AccessLevel
+import com.manydesigns.portofino.security.RequiresPermissions
+import net.sourceforge.stripes.action.DefaultHandler
+import net.sourceforge.stripes.action.ForwardResolution
+import net.sourceforge.stripes.action.Resolution
+import org.hibernate.Session
+import org.hibernate.criterion.Order
+import org.hibernate.criterion.Restrictions
+
+@RequiresPermissions(level = AccessLevel.VIEW)
+class ProjectsRecentlyUpdatedTicketsAction extends CustomAction {
+
+    @Inject(DatabaseModule.PERSISTENCE)
+    private Persistence persistence;
+
+    List tickets;
+
+    @DefaultHandler
+    public Resolution execute() {
+        Object component = ElementsThreadLocals.getOgnlContext().get("component");
+
+        Session session = persistence.getSession("tt");
+        tickets = session.createCriteria("tickets")
+                .addOrder(Order.desc("date_updated"))
+                .createCriteria("fk_affected_component_ticket")
+                .add(Restrictions.eq("component", component.id))
+                .setMaxResults(10)
+                .list();
+
+        return new ForwardResolution("/jsp/common/recently-updated-tickets.jsp");
+    }
+}
