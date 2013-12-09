be1dfcf2b4f539c8823e374996415ff3e62be3d1
Paolo Predonzani <paolo.predonzani@manydesigns.com>
2013-12-09 15:34:52 +0100

    #1494 demo-tt: permissions

--- demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/members/action.groovy
+++ demo-tt/src/main/webapp/WEB-INF/pages/projects/_detail/members/action.groovy
@@ -1,9 +1,14 @@
 package com.manydesigns.portofino.pageactions.crud
 
 import com.manydesigns.elements.ElementsThreadLocals
+import com.manydesigns.portofino.buttons.GuardType
+import com.manydesigns.portofino.buttons.annotations.Button
+import com.manydesigns.portofino.buttons.annotations.Buttons
+import com.manydesigns.portofino.buttons.annotations.Guard
 import com.manydesigns.portofino.security.AccessLevel
 import com.manydesigns.portofino.security.RequiresPermissions
 import com.manydesigns.portofino.security.SupportsPermissions
+import com.manydesigns.portofino.tt.TtUtils
 import net.sourceforge.stripes.action.Before
 import net.sourceforge.stripes.action.Resolution
 
@@ -19,62 +24,95 @@ class ProjectMembersAction extends CrudAction {
     }
 
     //**************************************************************************
-    // Extension hooks
+    // Role checking
     //**************************************************************************
+
+    public boolean isManager() {
+        return TtUtils.principalHasProjectRole(project, TtUtils.ROLE_MANAGER);
+    }
+
+    //**************************************************************************
+    // Create customizations
+    //**************************************************************************
+
+    @Override
+    @Button(list = "crud-search", key = "create.new", order = 1d, type = Button.TYPE_SUCCESS,
+            icon = "icon-plus icon-white", group = "crud")
+    @Guard(test="isManager()", type=GuardType.VISIBLE)
+    Resolution create() {
+        return super.create()
+    }
+
+    @Override
+    @Button(list = "crud-create", key = "save", order = 1d, type = Button.TYPE_PRIMARY)
+    @Guard(test="isManager()", type=GuardType.VISIBLE)
+    Resolution save() {
+        return super.save() 
+    }
 
     protected void createSetup(Object object) {
         object.project = project.id;
     }
 
-    protected boolean createValidate(Object object) {
-        return true;
+    //**************************************************************************
+    // Edit customizations
+    //**************************************************************************
+
+    @Override
+    @Buttons([
+        @Button(list = "crud-read", key = "edit", order = 1d, icon = "icon-edit icon-white",
+                group = "crud", type = Button.TYPE_SUCCESS),
+        @Button(list = "crud-read-default-button", key = "search")
+    ])
+    @Guard(test="isManager()", type=GuardType.VISIBLE)
+    Resolution edit() {
+        return super.edit()    //To change body of overridden methods use File | Settings | File Templates.
     }
 
-    protected void createPostProcess(Object object) {}
-
-
-    protected void editSetup(Object object) {}
-
-    protected boolean editValidate(Object object) {
-        return true;
+    @Override
+    @Button(list = "crud-edit", key = "update", order = 1d, type = Button.TYPE_PRIMARY)
+    @Guard(test="isManager()", type=GuardType.VISIBLE)
+    Resolution update() {
+        return super.update()    //To change body of overridden methods use File | Settings | File Templates.
     }
 
-    protected void editPostProcess(Object object) {}
+    //**************************************************************************
+    // Delete customizations
+    //**************************************************************************
 
-
-    protected boolean deleteValidate(Object object) {
-        return true;
+    @Override
+    @Button(list = "crud-read", key = "delete", order = 2d, icon = Button.ICON_TRASH, group = "crud")
+    @Guard(test = "isManager()", type = GuardType.VISIBLE)
+    public Resolution delete() {
+        return super.delete();
     }
 
-    protected void deletePostProcess(Object object) {}
+    //**************************************************************************
+    // Bulk edit customizations
+    //**************************************************************************
 
-
-    protected Resolution getBulkEditView() {
-        return super.getBulkEditView();
+    @Override
+    @Button(list = "crud-search", key = "edit", order = 2d, icon = Button.ICON_EDIT, group = "crud")
+    @Guard(test = "isBulkOperationsEnabled() && isManager()", type = GuardType.VISIBLE)
+    Resolution bulkEdit() {
+        return super.bulkEdit();
     }
 
-    protected Resolution getCreateView() {
-        return super.getCreateView();
+    @Button(list = "crud-bulk-edit", key = "update", order = 1d, type = Button.TYPE_PRIMARY)
+    @Guard(test = "isManager()", type = GuardType.VISIBLE)
+    Resolution bulkUpdate() {
+        return super.bulkUpdate();
     }
 
-    protected Resolution getEditView() {
-        return super.getEditView();
+    //**************************************************************************
+    // Bulk delete customizations
+    //**************************************************************************
+
+    @Button(list = "crud-search", key = "delete", order = 3d, icon = Button.ICON_TRASH, group = "crud")
+    @Guard(test = "isBulkOperationsEnabled() && isManager()", type = GuardType.VISIBLE)
+    public Resolution bulkDelete() {
+        throw new UnsupportedOperationException("Bulk operations not supported on versions");
     }
 
-    protected Resolution getReadView() {
-        return super.getReadView();
-    }
-
-    protected Resolution getSearchView() {
-        return super.getSearchView();
-    }
-
-    protected Resolution getEmbeddedSearchView() {
-        return super.getEmbeddedSearchView();
-    }
-
-    protected Resolution getSearchResultsPageView() {
-        return super.getSearchResultsPageView()
-    }
 
 }
