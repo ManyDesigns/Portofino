0735ad0ef7bac7b8cb6d33997eab4930e33dbd85
Paolo Predonzani <paolo.predonzani@manydesigns.com>
2013-12-18 12:40:38 +0100

    #1493: demo-tt/admin/users: customizations

--- demo-tt/src/main/webapp/WEB-INF/pages/admin/users/action.groovy
+++ demo-tt/src/main/webapp/WEB-INF/pages/admin/users/action.groovy
@@ -5,68 +5,19 @@ import com.manydesigns.portofino.security.RequiresPermissions
 import com.manydesigns.portofino.security.SupportsPermissions
 import net.sourceforge.stripes.action.Resolution
 
-@SupportsPermissions([ CrudAction.PERMISSION_CREATE, CrudAction.PERMISSION_EDIT, CrudAction.PERMISSION_DELETE ])
+@SupportsPermissions([CrudAction.PERMISSION_CREATE, CrudAction.PERMISSION_EDIT, CrudAction.PERMISSION_DELETE])
 @RequiresPermissions(level = AccessLevel.VIEW)
 class MyCrudAction extends CrudAction {
 
-    //Automatically generated on Mon Oct 28 13:17:10 CET 2013 by ManyDesigns Portofino
-    //Write your code here
-
-    //**************************************************************************
-    // Extension hooks
-    //**************************************************************************
-
-    protected void createSetup(Object object) {}
-
-    protected boolean createValidate(Object object) {
-        return true;
+    @Override
+    Resolution create() {
+        throw new UnsupportedOperationException("Users cannot be created");
     }
 
-    protected void createPostProcess(Object object) {}
-
-
-    protected void editSetup(Object object) {}
-
-    protected boolean editValidate(Object object) {
-        return true;
+    @Override
+    Resolution save() {
+        throw new UnsupportedOperationException("Users cannot be created");
     }
 
-    protected void editPostProcess(Object object) {}
-
-
-    protected boolean deleteValidate(Object object) {
-        return true;
-    }
-
-    protected void deletePostProcess(Object object) {}
-
-
-    protected Resolution getBulkEditView() {
-        return super.getBulkEditView();
-    }
-
-    protected Resolution getCreateView() {
-        return super.getCreateView();
-    }
-
-    protected Resolution getEditView() {
-        return super.getEditView();
-    }
-
-    protected Resolution getReadView() {
-        return super.getReadView();
-    }
-
-    protected Resolution getSearchView() {
-        return super.getSearchView();
-    }
-
-    protected Resolution getEmbeddedSearchView() {
-        return super.getEmbeddedSearchView();
-    }
-
-    protected Resolution getSearchResultsPageView() {
-        return super.getSearchResultsPageView()
-    }
 
 }
