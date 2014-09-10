c0a73b90a829c57fe237ddbce1309f780c1b5721
Alessio Stalla <alessio.stalla@manydesigns.com>
2014-09-10 18:20:16 +0200

    #PRT-12 HTML5 charts (with chart.js)

--- portofino-chart/src/main/resources/META-INF/resources/m/chart/chartjs/chartjs-configuration.js
+++ portofino-chart/src/main/resources/META-INF/resources/m/chart/chartjs/chartjs-configuration.js
@@ -5,153 +5,155 @@ var portofino = portofino || {};
 portofino.charts = portofino.charts || {};
 portofino.charts.chartjs = portofino.charts.chartjs || {};
 
+/* http://www.colorbrewer2.org/ */
+
 portofino.charts.chartjs.colorConfigurarions1D = [
     {
-        highlight: "rgba(204,82,82,0.2)",
-        color: "rgba(204,82,82,1)"
+        highlight: "rgba(141,211,199,0.5)",
+        color: "rgba(141,211,199,1)"
     },
     {
-        highlight: "rgba(82,204,82,0.2)",
-        color: "rgba(82,204,82,1)"
+        highlight: "rgba(255,255,179,0.5)",
+        color: "rgba(255,255,179,1)"
     },
     {
-        highlight: "rgba(82,82,204,0.2)",
-        color: "rgba(82,82,204,1)"
+        highlight: "rgba(190,186,218,0.5)",
+        color: "rgba(190,186,218,1)"
     },
     {
-        highlight: "rgba(217,217,87,0.2)",
-        color: "rgba(217,217,87,1)"
+        highlight: "rgba(251,128,114,0.5)",
+        color: "rgba(251,128,114,1)"
     },
     {
-        highlight: "rgba(87,217,217,0.2)",
-        color: "rgba(87,217,217,1)"
+        highlight: "rgba(128,177,211,0.5)",
+        color: "rgba(128,177,211,1)"
     },
     {
-        highlight: "rgba(217,87,217,0.2)",
-        color: "rgba(217,87,217,1)"
+        highlight: "rgba(253,180,98,0.5)",
+        color: "rgba(253,180,98,1)"
     },
     {
-        highlight: "rgba(212,148,85,0.2)",
-        color: "rgba(212,148,85,1)"
+        highlight: "rgba(179,222,105,0.5)",
+        color: "rgba(179,222,105,1)"
     },
     {
-        highlight: "rgba(148,212,85,0.2)",
-        color: "rgba(148,212,85,1)"
+        highlight: "rgba(252,205,229,0.5)",
+        color: "rgba(252,205,229,1)"
     },
     {
-        highlight: "rgba(85,212,148,0.2)",
-        color: "rgba(85,212,148,1)"
+        highlight: "rgba(217,217,217,0.5)",
+        color: "rgba(217,217,217,1)"
     },
     {
-        highlight: "rgba(85,148,212,0.2)",
-        color: "rgba(85,148,212,1)"
+        highlight: "rgba(188,128,189,0.5)",
+        color: "rgba(188,128,189,1)"
     },
     {
-        highlight: "rgba(148,85,212,0.2)",
-        color: "rgba(148,85,212,1)"
+        highlight: "rgba(204,235,197,0.5)",
+        color: "rgba(204,235,197,1)"
     },
     {
-        highlight: "rgba(212,85,148,0.2)",
-        color: "rgba(212,85,148,1)"
+        highlight: "rgba(255,237,111,0.5)",
+        color: "rgba(255,237,111,1)"
     }
 ];
 
 portofino.charts.chartjs.colorConfigurarions2D = [
     {
-        fillColor: "rgba(204,82,82,0.2)",
-        strokeColor: "rgba(204,82,82,1)",
-        pointColor: "rgba(204,82,82,1)",
+        fillColor: "rgba(141,211,199,0.5)",
+        strokeColor: "rgba(141,211,199,1)",
+        pointColor: "rgba(141,211,199,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(204,82,82,1)"
+        pointHighlightStroke: "rgba(141,211,199,1)"
     },
     {
-        fillColor: "rgba(82,204,82,0.2)",
-        strokeColor: "rgba(82,204,82,1)",
-        pointColor: "rgba(82,204,82,1)",
+        fillColor: "rgba(255,255,179,0.5)",
+        strokeColor: "rgba(255,255,179,1)",
+        pointColor: "rgba(255,255,179,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(82,204,82,1)"
+        pointHighlightStroke: "rgba(255,255,179,1)"
     },
     {
-        fillColor: "rgba(82,82,204,0.2)",
-        strokeColor: "rgba(82,82,204,1)",
-        pointColor: "rgba(82,82,204,1)",
+        fillColor: "rgba(190,186,218,0.5)",
+        strokeColor: "rgba(190,186,218,1)",
+        pointColor: "rgba(190,186,218,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(82,82,204,1)"
+        pointHighlightStroke: "rgba(190,186,218,1)"
     },
     {
-        fillColor: "rgba(217,217,87,0.2)",
-        strokeColor: "rgba(217,217,87,1)",
-        pointColor: "rgba(217,217,87,1)",
+        fillColor: "rgba(251,128,114,0.5)",
+        strokeColor: "rgba(251,128,114,1)",
+        pointColor: "rgba(251,128,114,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(217,217,87,1)"
+        pointHighlightStroke: "rgba(251,128,114,1)"
     },
     {
-        fillColor: "rgba(87,217,217,0.2)",
-        strokeColor: "rgba(87,217,217,1)",
-        pointColor: "rgba(87,217,217,1)",
+        fillColor: "rgba(128,177,211,0.5)",
+        strokeColor: "rgba(128,177,211,1)",
+        pointColor: "rgba(128,177,211,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(87,217,217,1)"
+        pointHighlightStroke: "rgba(128,177,211,1)"
     },
     {
-        fillColor: "rgba(217,87,217,0.2)",
-        strokeColor: "rgba(217,87,217,1)",
-        pointColor: "rgba(217,87,217,1)",
+        fillColor: "rgba(253,180,98,0.5)",
+        strokeColor: "rgba(253,180,98,1)",
+        pointColor: "rgba(253,180,98,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(217,87,217,1)"
+        pointHighlightStroke: "rgba(253,180,98,1)"
     },
     {
-        fillColor: "rgba(212,148,85,0.2)",
-        strokeColor: "rgba(212,148,85,1)",
-        pointColor: "rgba(212,148,85,1)",
+        fillColor: "rgba(179,222,105,0.5)",
+        strokeColor: "rgba(179,222,105,1)",
+        pointColor: "rgba(179,222,105,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(212,148,85,1)"
+        pointHighlightStroke: "rgba(179,222,105,1)"
     },
     {
-        fillColor: "rgba(148,212,85,0.2)",
-        strokeColor: "rgba(148,212,85,1)",
-        pointColor: "rgba(148,212,85,1)",
+        fillColor: "rgba(252,205,229,0.5)",
+        strokeColor: "rgba(252,205,229,1)",
+        pointColor: "rgba(252,205,229,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(148,212,85,1)"
+        pointHighlightStroke: "rgba(252,205,229,1)"
     },
     {
-        fillColor: "rgba(85,212,148,0.2)",
-        strokeColor: "rgba(85,212,148,1)",
-        pointColor: "rgba(85,212,148,1)",
+        fillColor: "rgba(217,217,217,0.5)",
+        strokeColor: "rgba(217,217,217,1)",
+        pointColor: "rgba(217,217,217,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(85,212,148,1)"
+        pointHighlightStroke: "rgba(217,217,217,1)"
     },
     {
-        fillColor: "rgba(85,148,212,0.2)",
-        strokeColor: "rgba(85,148,212,1)",
-        pointColor: "rgba(85,148,212,1)",
+        fillColor: "rgba(188,128,189,0.5)",
+        strokeColor: "rgba(188,128,189,1)",
+        pointColor: "rgba(188,128,189,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(85,148,212,1)"
+        pointHighlightStroke: "rgba(188,128,189,1)"
     },
     {
-        fillColor: "rgba(148,85,212,0.2)",
-        strokeColor: "rgba(148,85,212,1)",
-        pointColor: "rgba(148,85,212,1)",
+        fillColor: "rgba(204,235,197,0.5)",
+        strokeColor: "rgba(204,235,197,1)",
+        pointColor: "rgba(204,235,197,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(148,85,212,1)"
+        pointHighlightStroke: "rgba(204,235,197,1)"
     },
     {
-        fillColor: "rgba(212,85,148,0.2)",
-        strokeColor: "rgba(212,85,148,1)",
-        pointColor: "rgba(212,85,148,1)",
+        fillColor: "rgba(255,237,111,0.5)",
+        strokeColor: "rgba(255,237,111,1)",
+        pointColor: "rgba(255,237,111,1)",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(212,85,148,1)"
+        pointHighlightStroke: "rgba(255,237,111,1)"
     }];
 
 portofino.charts.chartjs.create = function (chartId, chartKind, chartJsMethod, data) {
