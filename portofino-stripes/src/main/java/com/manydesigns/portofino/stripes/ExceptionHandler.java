92ef702b1195ec192f1136d32ae0e0f95016015c
Alessio Stalla <alessio.stalla@manydesigns.com>
2014-10-17 09:27:19 +0200

    StackOverflow safe handling - omit stack, provide some contextual information

--- portofino-stripes/src/main/java/com/manydesigns/portofino/stripes/ExceptionHandler.java
+++ portofino-stripes/src/main/java/com/manydesigns/portofino/stripes/ExceptionHandler.java
@@ -2,13 +2,24 @@ package com.manydesigns.portofino.stripes;
 
 import net.sourceforge.stripes.exception.DefaultExceptionHandler;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ExceptionHandler extends DefaultExceptionHandler {
 
     public void handle(StackOverflowError ex, HttpServletRequest req, HttpServletResponse resp) {
-        throw new RuntimeException("Stack Overflow! Exception swallowed to prevent log flooding. Some context: " + req.getRequestURI() + (req.getQueryString() != null ? req.getQueryString() : ""));
+        throw new RuntimeException(
+                "Stack Overflow! Exception swallowed to prevent log flooding." +
+                        " Request URL: " + req.getRequestURL() +
+                        " Query string: " + req.getQueryString() +
+                        " Include path: " +  req.getAttribute(RequestDispatcher.INCLUDE_SERVLET_PATH) +
+                        " Forward string: " + req.getAttribute(RequestDispatcher.FORWARD_SERVLET_PATH)) {
+            @Override
+            public Throwable fillInStackTrace() {
+                return this;
+            }
+        };
     }
 
 }
