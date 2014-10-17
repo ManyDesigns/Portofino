4def064570f5153f4506504f93991765fd6174ab
Alessio Stalla <alessio.stalla@manydesigns.com>
2014-10-17 09:59:11 +0200

    StackOverflow safe handling - omit stack, provide some contextual information

--- portofino-stripes/src/main/java/com/manydesigns/portofino/stripes/ExceptionHandler.java
+++ portofino-stripes/src/main/java/com/manydesigns/portofino/stripes/ExceptionHandler.java
@@ -3,18 +3,30 @@ package com.manydesigns.portofino.stripes;
 import net.sourceforge.stripes.exception.DefaultExceptionHandler;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletRequestWrapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ExceptionHandler extends DefaultExceptionHandler {
 
     public void handle(StackOverflowError ex, HttpServletRequest req, HttpServletResponse resp) {
-        throw new RuntimeException(
-                "Stack Overflow! Exception swallowed to prevent log flooding." +
-                        " Request URL: " + req.getRequestURL() +
-                        " Query string: " + req.getQueryString() +
-                        " Include path: " +  req.getAttribute(RequestDispatcher.INCLUDE_SERVLET_PATH) +
-                        " Forward string: " + req.getAttribute(RequestDispatcher.FORWARD_SERVLET_PATH)) {
+        String contextInfo = " Request: " + req;
+        if(req instanceof ServletRequestWrapper) {
+            contextInfo += " Wrapped request: " + ((ServletRequestWrapper) req).getRequest();
+        }
+        try {
+            contextInfo += " Request URL: " + req.getRequestURL();
+        } catch (Throwable e) {}
+        try {
+            contextInfo += " Query string: " + req.getQueryString();
+        } catch (Throwable e) {}
+        try {
+            contextInfo += " Include path: " + req.getAttribute(RequestDispatcher.INCLUDE_SERVLET_PATH);
+        } catch (Throwable e) {}
+        try {
+            contextInfo += " Forward string: " + req.getAttribute(RequestDispatcher.FORWARD_SERVLET_PATH);
+        } catch (Throwable e) {}
+        throw new RuntimeException("Stack Overflow! Exception swallowed to prevent log flooding." + contextInfo) {
             @Override
             public Throwable fillInStackTrace() {
                 return this;
