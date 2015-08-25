cf7fcf7ddf99e61d7943cab1c25adef34f5fc471
Emanuele Poggi <emanuele.poggi@manydesigns.com>
2015-08-25 11:58:40 +0200

    Example page

---
+++ portofino-war-archetype/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/pages/example/gallery/action.groovy
@@ -0,0 +1,18 @@
+package archetype
+
+import com.manydesigns.portofino.security.*
+
+@RequiresPermissions(level = AccessLevel.VIEW)
+class MyGallery extends GalleryAction {
+
+    //Automatically generated on Tue Aug 25 10:12:39 CEST 2015 by ManyDesigns Portofino
+    //Example below. Adapt it to your needs.
+
+    @Override
+    void loadImages() {
+        gallery.addImage( new Image("Image 1","Image 1","","","/m/gallery/images/image1.png","/m/gallery/images/image1_thumb.jpg"))
+        gallery.addImage( new Image("Image 2","Image 2","","","/m/gallery/images/image2.png","/m/gallery/images/image2_thumb.jpg"))
+        gallery.addImage( new Image("Image 3","Image 3","","","/m/gallery/images/image3.png","/m/gallery/images/image3_thumb.jpg"))
+    }
+
+}
---
+++ portofino-war-archetype/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/pages/example/map/action.groovy
@@ -0,0 +1,25 @@
+package archetype
+
+import com.manydesigns.portofino.security.*
+
+@RequiresPermissions(level = AccessLevel.VIEW)
+class MyMap extends MapAction {
+
+    //Automatically generated on Tue Aug 25 10:08:38 CEST 2015 by ManyDesigns Portofino
+    //Example below. Adapt it to your needs.
+
+    @Override
+    void loadObjects() {
+        loadMarkers();
+    }
+
+    def loadMarkers() {
+
+        Position markerPosition = new Position( new BigDecimal( 44.405650), new BigDecimal( 8.946256 ));
+        map.addMarker( new Marker( "Genova", "ManyDesigns Office", markerPosition, "http://www.manydesigns.com"));
+
+        markerPosition = new Position( new BigDecimal( 44.3031559), new BigDecimal(9.2097879 ));
+        map.addMarker( new Marker( "Portofino", "Its purpose is to help developers create outstanding enterprise applications.", markerPosition, "http://portofino.manydesigns.com/"));
+    }
+
+}
