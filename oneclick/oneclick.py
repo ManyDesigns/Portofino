560a351c8199ada7b03560c33ccc19e03a026697
alessio.stalla@manydesigns.com <alessio.stalla@manydesigns.com>
2015-12-11 17:40:33 +0100

    Portofino 4.2.1. Fix oneclick.py on Linux.

--- oneclick/oneclick.py
+++ oneclick/oneclick.py
@@ -48,9 +48,7 @@ if(os.path.exists(tomcat_path)):
 os.system("unzip " + tomcat_zip + " -d " + base_path)
 
 shutil.rmtree(tomcat_path + "/webapps/ROOT")
-shutil.rmtree(tomcat_path + "/webapps/examples") #Conflicts with portofino /examples
-
-shutil.copy(portofino_path + "/README.MD", base_path + "/README.MD")
+shutil.copy(portofino_path + "/README.md", base_path + "/README.md")
 shutil.copy(portofino_path + "/COPYRIGHT.txt", base_path + "/COPYRIGHT.txt")
 shutil.copy(portofino_path + "/LICENSE.txt", base_path + "/LICENSE.txt")
 shutil.copy(portofino_path + "/THIRDPARTIES.txt", base_path + "/THIRDPARTIES.txt")
@@ -59,14 +57,15 @@ print "Downloading JDBC drivers..."
 for driver in local.drivers:
     urllib.urlretrieve("http://repo1.maven.org/maven2/" + driver[0] + driver[1], tomcat_path + "/lib/" + driver[1])
 
-print "Building Portofino..."
-os.system("pushd " + portofino_path + "; mvn clean install; popd")
+command = "cd " + portofino_path + "; mvn clean install"
+print "Building Portofino: " + command
+os.system(command)
 
 print "Generating oneclick from archetype..."
-os.system("pushd " + build_path + "; mvn archetype:generate -DarchetypeArtifactId=portofino-war-archetype -DarchetypeGroupId=com.manydesigns -DarchetypeVersion=" + local.portofino_version + " -DinteractiveMode=false -DgroupId=com.manydesigns -DartifactId=portofino-oneclick -Dversion=" + local.portofino_version + "; popd")
+os.system("cd " + build_path + "; mvn archetype:generate -DarchetypeArtifactId=portofino-war-archetype -DarchetypeGroupId=com.manydesigns -DarchetypeVersion=" + local.portofino_version + " -DinteractiveMode=false -DgroupId=com.manydesigns -DartifactId=portofino-oneclick -Dversion=" + local.portofino_version + "")
 
 print "Building oneclick..."
-os.system("pushd " + oneclick_path + "; mvn clean package; popd")
+os.system("cd " + oneclick_path + "; mvn clean package")
 
 
 shutil.copy(oneclick_path + "/target/portofino-oneclick-" + local.portofino_version + ".war", tomcat_path + "/webapps/ROOT.war")
