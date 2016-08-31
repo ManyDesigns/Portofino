8fdaa111dfd3fd0591e4baabf11034491acd3c89
Manuel Durán Aguete <manuel@aguete.org>
2016-08-31 08:33:18 +0200

    Added license comment

--- portofino-db2/src/main/java/com/manydesigns/portofino/database/platforms/IbmDb2ZosDatabasePlatform.java
+++ portofino-db2/src/main/java/com/manydesigns/portofino/database/platforms/IbmDb2ZosDatabasePlatform.java
@@ -1,8 +1,36 @@
+/*
+ * Copyright (C) 2005-2016 ManyDesigns srl.  All rights reserved.
+ * http://www.manydesigns.com/
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package com.manydesigns.portofino.database.platforms;
 
 import com.manydesigns.portofino.model.database.ConnectionProvider;
 import com.manydesigns.portofino.database.dialects.DB2ZOSDialect;
 
+
+/*
+ * @author Angelo Lupo          - angelo.lupo@manydesigns.com
+ * @author Giampiero Granatella - giampiero.granatella@manydesigns.com
+ * @author Emanuele Poggi       - emanuele.poggi@manydesigns.com
+ * @author Alessio Stalla       - alessio.stalla@manydesigns.com
+ * @author Manuel Durán Aguete  - manuel@aguete.org
+*/
 public class IbmDb2ZosDatabasePlatform extends AbstractDatabasePlatform{
     public static final String copyright =
             "Copyright (C) 2005-2016, ManyDesigns srl";
