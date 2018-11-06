b4bd1fac41de6daeb4aff0c9359065fb014aad59
Alessio Stalla <alessiostalla@gmail.com>
2018-11-06 15:33:16 +0100

    Update Angular dependencies. Update "attachments" page in demo-tt to Portofino 5.

--- demo-tt/src/main/webapp/WEB-INF/actions/projects/_detail/tickets/_detail/attachments/action.groovy
+++ demo-tt/src/main/webapp/WEB-INF/actions/projects/_detail/tickets/_detail/attachments/action.groovy
@@ -1,17 +1,10 @@
 package com.manydesigns.portofino.pageactions.crud
 
-import com.manydesigns.portofino.tt.TtUtils
-
 import com.manydesigns.elements.ElementsThreadLocals
-import com.manydesigns.portofino.buttons.GuardType
-import com.manydesigns.portofino.buttons.annotations.Button
-import com.manydesigns.portofino.buttons.annotations.Buttons
-import com.manydesigns.portofino.buttons.annotations.Guard
 import com.manydesigns.portofino.security.AccessLevel
 import com.manydesigns.portofino.security.RequiresPermissions
 import com.manydesigns.portofino.security.SupportsPermissions
-import net.sourceforge.stripes.action.Before
-import net.sourceforge.stripes.action.Resolution
+import com.manydesigns.portofino.tt.TtUtils
 import org.apache.shiro.SecurityUtils
 import org.apache.shiro.subject.Subject
 
@@ -22,8 +15,9 @@ class TicketAttachementsCrudAction extends CrudAction {
     Object project;
     Object ticket;
 
-    @Before
-    public void prepareProject() {
+    @Override
+    void prepareForExecution() {
+        super.prepareForExecution()
         project = ElementsThreadLocals.getOgnlContext().get("project");
         ticket = ElementsThreadLocals.getOgnlContext().get("ticket");
     }
@@ -58,20 +52,9 @@ class TicketAttachementsCrudAction extends CrudAction {
     //**************************************************************************
 
     @Override
-    @Button(list = "crud-search", key = "create.new", order = 1d, type = Button.TYPE_SUCCESS,
-            icon = "glyphicon-plus white")
-    @Guard(test="canEditTicket()", type=GuardType.VISIBLE)
-    Resolution create() {
-        return super.create()    //To change body of overridden methods use File | Settings | File Templates.
+    boolean isCreateEnabled() {
+        return canEditTicket()
     }
-
-    @Override
-    @Button(list = "crud-create", key = "save", order = 1d, type = Button.TYPE_PRIMARY)
-    @Guard(test="canEditTicket()", type=GuardType.VISIBLE)
-    Resolution save() {
-        return super.save()    //To change body of overridden methods use File | Settings | File Templates.
-    }
-
 
     protected void createSetup(Object object) {
         object.project = ticket.project;
@@ -106,21 +89,8 @@ class TicketAttachementsCrudAction extends CrudAction {
     //**************************************************************************
 
     @Override
-    @Buttons([
-        @Button(list = "crud-read", key = "edit", order = 1d, icon = "glyphicon-edit white",
-                group = "crud", type = Button.TYPE_SUCCESS),
-        @Button(list = "crud-read-default-button", key = "search")
-    ])
-    @Guard(test="canEditTicket()", type=GuardType.VISIBLE)
-    Resolution edit() {
-        return super.edit()    //To change body of overridden methods use File | Settings | File Templates.
-    }
-
-    @Override
-    @Button(list = "crud-edit", key = "update", order = 1d, type = Button.TYPE_PRIMARY)
-    @Guard(test="canEditTicket()", type=GuardType.VISIBLE)
-    Resolution update() {
-        return super.update()    //To change body of overridden methods use File | Settings | File Templates.
+    boolean isEditEnabled() {
+        canEditTicket()
     }
 
     protected void editPostProcess(Object object) {
@@ -151,10 +121,9 @@ class TicketAttachementsCrudAction extends CrudAction {
     // Delete customizations
     //**************************************************************************
 
-    @Button(list = "crud-read", key = "delete", order = 2d, icon = Button.ICON_TRASH)
-    @Guard(test = "canEditTicket()", type = GuardType.VISIBLE)
-    public Resolution delete() {
-        return super.delete();
+    @Override
+    boolean isDeleteEnabled() {
+        canEditTicket()
     }
 
     protected void deletePostProcess(Object object) {
@@ -179,32 +148,6 @@ class TicketAttachementsCrudAction extends CrudAction {
                 null
         );
 
-    }
-
-    //**************************************************************************
-    // Bulk edit customizations
-    //**************************************************************************
-
-    @Button(list = "crud-bulk", key = "edit", order = 2d, icon = Button.ICON_EDIT)
-    @Guard(test = "isBulkOperationsEnabled() && canEditTicket()", type = GuardType.VISIBLE)
-    Resolution bulkEdit() {
-        return super.bulkEdit()
-    }
-
-    @Button(list = "crud-bulk", key = "update", order = 1d, type = Button.TYPE_PRIMARY)
-    @Guard(test = "canEditTicket()", type = GuardType.VISIBLE)
-    Resolution bulkUpdate() {
-        return super.bulkUpdate()
-    }
-
-    //**************************************************************************
-    // Bulk delete customizations
-    //**************************************************************************
-
-    @Button(list = "crud-search", key = "delete", order = 3d, icon = Button.ICON_TRASH)
-    @Guard(test = "isBulkOperationsEnabled() && canEditTicket()", type = GuardType.VISIBLE)
-    public Resolution bulkDelete() {
-        return super.bulkDelete()
     }
 
 }
