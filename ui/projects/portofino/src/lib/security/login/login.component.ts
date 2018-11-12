f74d40a87439a6667327bf423913c619bd323231
Emanuele Poggi <emanuele.poggi@manydesigns.com>
2018-11-12 14:26:33 +0100

    Breadcrumbs component ad mat-card for detail,edit and bulk-edit

--- ui/projects/portofino/src/lib/security/login/login.component.ts
+++ ui/projects/portofino/src/lib/security/login/login.component.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {PortofinoService} from "../../portofino.service";
 import {MatDialogRef} from "@angular/material";
 import {AuthenticationService} from "../authentication.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'portofino-login',
@@ -11,11 +12,25 @@ import {AuthenticationService} from "../authentication.service";
 })
 export class LoginComponent implements OnInit {
 
+  public loginRecord:       FormGroup;
+  formErrors: any;
+
   username: string;
   password: string;
   message: string;
 
-  constructor(private dialogRef: MatDialogRef<LoginComponent>, private authenticationService: AuthenticationService) {}
+  constructor(private dialogRef: MatDialogRef<LoginComponent>, private authenticationService: AuthenticationService,
+              private builder: FormBuilder) {
+    this.loginRecord = this.builder.group({
+      username:              ['', Validators.required],
+      password:                  ['', Validators.required]
+    });
+
+    this.formErrors = {
+      username:        {},
+      password:            {},
+    };
+  }
 
   ngOnInit() {
   }
