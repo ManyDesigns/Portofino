be067e56a142824582f27dd466792d5203729d7f
Emanuele Poggi <emanuele.poggi@manydesigns.com>
2019-02-20 13:51:02 +0100

    Update env Portofino One click build

--- oneclick/setenv.bat
+++ oneclick/setenv.bat
@@ -1,1 +1,1 @@
-set JAVA_OPTS=-Xmx512m -XX:MaxPermSize=256m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -Djava.awt.headless=true %JAVA_OPTS%
+set JAVA_OPTS=-Dfile.encoding=UTF-8 -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -Djava.awt.headless=true %JAVA_OPTS%
