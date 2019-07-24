1b13ff7ddcacc533ba0108fcc38df91cc129bd41
Alessio Stalla <alessiostalla@gmail.com>
2019-07-24 11:17:13 +0200

    Quartz jobs improvements: #47 Close sessions even when the job throws #57 Reloadable Quartz jobs in Groovy

--- portofino-quartz/src/main/java/com/manydesigns/portofino/quartz/PortofinoJobFactory.java
+++ portofino-quartz/src/main/java/com/manydesigns/portofino/quartz/PortofinoJobFactory.java
@@ -29,9 +29,13 @@
 
 package com.manydesigns.portofino.quartz;
 
+import com.manydesigns.portofino.di.Inject;
 import com.manydesigns.portofino.di.Injections;
+import com.manydesigns.portofino.modules.BaseModule;
 import com.manydesigns.portofino.modules.DatabaseModule;
 import com.manydesigns.portofino.persistence.Persistence;
+import groovy.lang.GroovyObject;
+import groovy.util.GroovyScriptEngine;
 import org.quartz.*;
 import org.quartz.simpl.SimpleJobFactory;
 import org.quartz.spi.TriggerFiredBundle;
@@ -39,6 +43,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
+import java.io.File;
 
 /**
  * @author Paolo Predonzani     - paolo.predonzani@manydesigns.com
@@ -50,35 +55,70 @@ public class PortofinoJobFactory extends SimpleJobFactory {
     public static final String copyright =
             "Copyright (C) 2005-2017 ManyDesigns srl";
 
-    private final ServletContext servletContext;
     private static final Logger logger = LoggerFactory.getLogger(PortofinoJobFactory.class);
+    private final ServletContext servletContext;
+    @Inject(BaseModule.GROOVY_CLASS_PATH)
+    private File groovyClassPath;
+    @Inject(BaseModule.GROOVY_SCRIPT_ENGINE)
+    private GroovyScriptEngine groovyScriptEngine;
 
     public PortofinoJobFactory(ServletContext servletContext) {
         this.servletContext = servletContext;
+        Injections.inject(this, servletContext, null);
     }
 
     @Override
     public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler) throws SchedulerException {
-        final Job job = super.newJob(bundle, scheduler);
-        Injections.inject(job, servletContext, null);
+        Job job = instantiateJob(bundle);
         return new Job() {
             @Override
             public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-                job.execute(jobExecutionContext);
                 try {
-                    //In a different class to make the database module optional at runtime
-                    SessionCleaner.closeSessions(servletContext);
-                } catch (NoClassDefFoundError e) {
-                    logger.debug("Database module not available, not closing sessions", e);
+                    job.execute(jobExecutionContext);
+                } finally {
+                    try {
+                        //In a different class to make the database module optional at runtime
+                        SessionCleaner.closeSessions(servletContext);
+                    } catch (NoClassDefFoundError e) {
+                        logger.debug("Database module not available, not closing sessions", e);
+                    }
                 }
             }
         };
     }
 
+    protected Job instantiateJob(TriggerFiredBundle bundle) throws SchedulerException {
+        JobDetail jobDetail = bundle.getJobDetail();
+        Class jobClass = jobDetail.getJobClass();
+        if(GroovyObject.class.isAssignableFrom(jobClass)) {
+            //Attempt to reload
+            String classFileName = jobClass.getName().replace('.', File.separatorChar) + ".groovy";
+            try {
+                File groovyFile = new File(groovyClassPath, classFileName);
+                Class newClass = groovyScriptEngine.loadScriptByName(groovyFile.toURI().toString());
+                if(Job.class.isAssignableFrom(newClass)) {
+                    jobClass = newClass;
+                } else {
+                    logger.error(newClass + " does not extend " + Job.class + ", not reloading the job.");
+                }
+            } catch (Exception e) {
+                logger.error("Failed to reload job class " + jobClass.getName(), e);
+            }
+        }
+        Job job;
+        try {
+            job = (Job) jobClass.newInstance();
+        } catch (Exception e) {
+            throw new SchedulerException("Problem instantiating class '" + jobDetail.getJobClass().getName() + "'", e);
+        }
+        Injections.inject(job, servletContext, null);
+        return job;
+    }
+
 }
 
 class SessionCleaner {
-    public static void closeSessions(ServletContext servletContext) {
+    static void closeSessions(ServletContext servletContext) {
         Persistence persistence = (Persistence) servletContext.getAttribute(DatabaseModule.PERSISTENCE);
         persistence.closeSessions();
     }
