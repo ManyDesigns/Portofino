789dd1b82eced3d3b79b05d07e7ea7beddb0a470
Alessio Stalla <alessiostalla@gmail.com>
2019-12-13 11:39:41 +0100

    Update Liquibase to latest version and start updating Angular

--- portofino-database/src/test/resources/com/manydesigns/portofino/database/sql/hibernatetest.sql
+++ portofino-database/src/test/resources/com/manydesigns/portofino/database/sql/hibernatetest.sql
@@ -200,7 +200,7 @@ CREATE TABLE test_no_pk(id bigint);
 DROP VIEW IF EXISTS public.test_view_1;
 CREATE VIEW public.test_view_1 AS SELECT * from public.table3;
 
-DROP TABLE IF EXISTS public.test_spaces; --it looks like Liquibase doesn't find the pk if the table name is escaped
+DROP TABLE IF EXISTS public.test_spaces; --it looks like Liquibase doesn't find the pk if the table name is escaped. TODO we should test for the table name to contain spaces as well when Liquibase will support it.
 CREATE TABLE public.test_spaces(
     "id spaces" bigint not null primary key,
     "some other column with spaces" character varying (123));
