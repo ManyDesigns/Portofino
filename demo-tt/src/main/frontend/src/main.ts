24935bc0ed80d5d9ae647ce7d88b9e9d8d34f114
alessiostalla@gmail.com <alessiostalla@gmail.com>
2020-02-27 14:54:50 +0100

    Angular 9 demo-tt

--- demo-tt/src/main/frontend/src/main.ts
+++ demo-tt/src/main/frontend/src/main.ts
@@ -1,10 +1,5 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import "@angular/compiler"; //Otherwise: Error: "Angular JIT compilation failed: '@angular/compiler' not loaded!
-//  - JIT compilation is discouraged for production use-cases! Consider AOT mode instead.
-//  - Did you bootstrap using '@angular/platform-browser-dynamic' or '@angular/platform-server'?
-//  - Alternatively provide the compiler with 'import "@angular/compiler";' before bootstrapping."
-//TODO can we avoid this?
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import { DemoTTAppModule } from './app/app.module';
 import { environment } from './environments/environment';
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {Component, NgModule} from '@angular/core';
 import {
-  PortofinoModule, NAVIGATION_COMPONENT, DefaultNavigationComponent, PortofinoUpstairsModule} from "portofino";
+  PortofinoModule, PortofinoUpstairsModule} from "portofino";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
