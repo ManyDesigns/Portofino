080a82a2efe907566738f9af9462900f2f79b4c7
Alessio Stalla <alessiostalla@gmail.com>
2020-07-02 22:56:09 +0200

    #379 Restore profile page of demo-tt

--- demo-tt/src/main/frontend/src/app/profile.component.ts
+++ demo-tt/src/main/frontend/src/app/profile.component.ts
@@ -1,13 +1,15 @@
 import {
-  AuthenticationService, DATE_TYPE,
-  Form, forObject,
+  AuthenticationService, Button,
+  DATE_TYPE,
+  Form,
+  forObject,
   NotificationService,
   Page,
   PortofinoComponent,
   PortofinoService
 } from "portofino";
 import {Component, OnInit} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
 import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
@@ -19,22 +21,42 @@ import {FormGroup} from "@angular/forms";
   template: `
     <portofino-page-layout [page]="this">
       <ng-template #content>
-        <mat-card>
-          <mat-card-header>
-            <img mat-card-avatar [src]="photo" alt="User avatar">
-            <mat-card-title>{{authenticationService.currentUser.displayName}}</mat-card-title>
-          </mat-card-header>
-          <mat-card-content>
-            <portofino-form [controls]="form" [form]="formDefinition"
-                            fxLayout="row wrap" fxLayoutGap="20px" fxLayoutAlign="default center"></portofino-form>
-          </mat-card-content>
-        </mat-card>
+        <div gdAreas="content sidebar" gdColumns="auto 300px" gdGap="10px">
+          <mat-card gdArea="content">
+            <mat-card-header>
+              <img mat-card-avatar [src]="photo" alt="User avatar">
+              <mat-card-title>{{authenticationService.currentUser.displayName}}</mat-card-title>
+            </mat-card-header>
+            <mat-card-content>
+              <portofino-form [controls]="form" [form]="formDefinition"
+                              fxLayout="row wrap" fxLayoutGap="20px" fxLayoutAlign="default center"></portofino-form>
+            </mat-card-content>
+            <mat-card-actions *ngIf="editMode">
+              <portofino-buttons [component]="this" list="edit"></portofino-buttons>
+            </mat-card-actions>
+          </mat-card>
+          <mat-card gdArea="sidebar">
+            <mat-card-title>{{'Actions' | translate}}</mat-card-title>
+            <mat-card-content>
+              <div fxLayout="column" fxLayoutGap="10px">
+                <span><!-- Divider --></span>
+                <a mat-raised-button color="accent" (click)="authenticationService.showChangePasswordDialog()">
+                  Change your password
+                </a>
+                <a mat-raised-button color="primary" (click)="edit()">Edit your data</a>
+                <a mat-raised-button color="warn" (click)="photoForm()">Change your photo</a>
+              </div>
+            </mat-card-content>
+          </mat-card>
+        </div>
       </ng-template>
     </portofino-page-layout>`
 })
 export class ProfileComponent extends Page implements OnInit {
   photo: SafeResourceUrl;
   formDefinition = new Form();
+  user: any;
+  editMode: boolean;
   readonly form = new FormGroup({});
 
   constructor(
@@ -48,16 +70,10 @@ export class ProfileComponent extends Page implements OnInit {
   }
 
   ngOnInit(): void {
-    this.http.get<any>(this.portofino.apiRoot + "profile/view").subscribe(u => {
-      let form = Form.fromClassAccessor(forObject(u, {
-        types: {
-          "registration": DATE_TYPE,
-          "last_access": DATE_TYPE,
-          "validated": DATE_TYPE,
-        }
-      }), { object: u });
-      form.editable = false;
-      this.formDefinition = form;
+    this.http.get<any>(this.portofino.apiRoot + "profile").subscribe(u => {
+      this.user = u;
+      this.setupForm(this.user);
+      this.formDefinition.editable = false;
     });
     this.http.get(this.portofino.apiRoot + 'profile/photo', { responseType: "blob" }).subscribe(data => {
       let reader = new FileReader();
@@ -68,4 +84,43 @@ export class ProfileComponent extends Page implements OnInit {
     });
   }
 
+  edit() {
+    this.setupForm(this.user, [ "first_name", "last_name" ]);
+    this.formDefinition.editable = true;
+    this.editMode = true;
+  }
+
+  @Button({ list: 'edit', text: "Save", color: "primary" })
+  save() {
+    this.http.put(
+      this.portofino.apiRoot + "profile",
+      new HttpParams({ fromObject: this.form.getRawValue() })).subscribe(u => {
+      this.user = u;
+      this.cancel();
+    }, () => {
+      this.notificationService.error("Update failed");
+    });
+  }
+
+  @Button({ list: 'edit', text: "Cancel" })
+  @Button({ list: 'photo', text: "Cancel" })
+  cancel() {
+    this.setupForm(this.user);
+    this.formDefinition.editable = false;
+    this.editMode = false;
+  }
+
+  photoForm() {
+    this.notificationService.error("Not yet implemented");
+  }
+
+  protected setupForm(user, properties?: string[]) {
+    this.formDefinition = Form.fromClassAccessor(forObject(user, {
+      types: {
+        "registration": DATE_TYPE,
+        "last_access": DATE_TYPE,
+        "validated": DATE_TYPE,
+      }
+    }), { object: user, properties });
+  }
 }
