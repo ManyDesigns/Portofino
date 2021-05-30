8d8e47402f7cb5c42cbcef6bd538136392583f06
Alessio Stalla <alessiostalla@gmail.com>
2021-05-30 19:35:41 +0200

    Close statements in sample schema based multitenancy implementation

--- demo-tt/src/main/java/com/manydesigns/portofino/tt/SampleSchemaBasedMultiTenantImplementation.java
+++ demo-tt/src/main/java/com/manydesigns/portofino/tt/SampleSchemaBasedMultiTenantImplementation.java
@@ -9,16 +9,19 @@ import org.apache.shiro.subject.Subject;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SampleSchemaBasedMultiTenantImplementation extends SchemaBasedMultiTenancy {
 
     @Override
     protected void setSchema(Connection connection, String tenant) throws SQLException {
         //This is just an example to test that Hibernate effectively switches schema.
-        if(tenant.equals(getDefaultTenant())) {
-            connection.createStatement().execute("set search_path to tt;");
-        } else {
-            connection.createStatement().execute("set search_path to tt2;");
+        try(Statement stmt = connection.createStatement()) {
+            if (tenant.equals(getDefaultTenant())) {
+                stmt.execute("set search_path to tt;");
+            } else {
+                stmt.execute("set search_path to tt2;");
+            }
         }
     }
 
