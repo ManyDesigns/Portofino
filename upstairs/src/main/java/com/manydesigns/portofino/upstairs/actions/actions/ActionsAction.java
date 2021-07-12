6f7126f20e4604cbf5afc2531e1d2decf4f513dc
Alessio Stalla <alessiostalla@gmail.com>
2021-07-12 21:29:20 +0200

    Provide information about resource actions

--- upstairs/src/main/java/com/manydesigns/portofino/upstairs/actions/actions/ActionsAction.java
+++ upstairs/src/main/java/com/manydesigns/portofino/upstairs/actions/actions/ActionsAction.java
@@ -13,8 +13,11 @@ import com.manydesigns.portofino.resourceactions.ActionInstance;
 import com.manydesigns.portofino.resourceactions.ConfigurationWithDefaults;
 import com.manydesigns.portofino.resourceactions.ResourceAction;
 import com.manydesigns.portofino.resourceactions.registry.ActionInfo;
+import com.manydesigns.portofino.resourceactions.registry.ActionRegistry;
 import com.manydesigns.portofino.security.AccessLevel;
 import com.manydesigns.portofino.security.RequiresAdministrator;
+import com.manydesigns.portofino.security.RequiresPermissions;
+import com.manydesigns.portofino.upstairs.actions.support.ActionTypeInfo;
 import ognl.OgnlContext;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.vfs2.AllFileSelector;
@@ -22,12 +25,14 @@ import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequiresAdministrator
@@ -36,6 +41,9 @@ public class ActionsAction extends AbstractResourceAction {
     private static final Logger logger = LoggerFactory.getLogger(ActionsAction.class);
 
     public static final String PORTOFINO_ACTION_MOVE_TYPE = "application/vnd.com.manydesigns.portofino.action-move";
+
+    @Autowired
+    protected ActionRegistry actionRegistry;
 
     public ActionsAction() {
         minParameters = 0;
@@ -50,6 +58,23 @@ public class ActionsAction extends AbstractResourceAction {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
         return resource;
+    }
+
+    @Path(":types")
+    @GET
+    @RequiresPermissions(level = AccessLevel.NONE)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, ActionTypeInfo> getResourceActionTypes() {
+        Map<String, ActionTypeInfo> result = new HashMap<>();
+        actionRegistry.iterator().forEachRemaining(a -> {
+            String className = a.actionClass.getName();
+            result.put(a.description, new ActionTypeInfo(
+                    className,
+                    ElementsThreadLocals.getTextProvider().getText(className),
+                    ElementsThreadLocals.getTextProvider().getTextOrNull(className + ".description"),
+                    a.supportsDetail));
+        });
+        return result;
     }
 
     @GET
@@ -101,7 +126,10 @@ public class ActionsAction extends AbstractResourceAction {
         FileObject directory = parentActionInstance.getChildPageDirectory(segment);
         if(directory.exists()) {
             logger.error("Can't create actionDescriptor - directory {} exists", directory.getName().getPath());
-            throw new WebApplicationException("error.creating.page.the.directory.already.exists");
+            throw new WebApplicationException(
+                    Response.serverError()
+                    .entity(ElementsThreadLocals.getText("error.creating.page.the.directory.already.exists"))
+                    .build());
         }
         directory.createFolder();
         logger.debug("Creating the new child actionDescriptor in directory: {}", directory);
@@ -119,6 +147,7 @@ public class ActionsAction extends AbstractResourceAction {
             logger.debug("Creating _detail directory: {}", detailDir);
             detailDir.createFolder();
         }
+        logger.info("Created action of type " + actionClassName + " in directory " + directory);
     }
 
     @POST
