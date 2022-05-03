ba0869aa6b0632285d844fa55ad2fb33c43f1985
Alessio Stalla <alessiostalla@gmail.com>
2022-05-03 09:36:45 +0200

    #551 Configuration abstraction

--- portofino-crud/src/main/java/com/manydesigns/portofino/resourceactions/crud/configuration/database/CrudConfiguration.java
+++ portofino-crud/src/main/java/com/manydesigns/portofino/resourceactions/crud/configuration/database/CrudConfiguration.java
@@ -105,7 +105,7 @@ public class CrudConfiguration extends com.manydesigns.portofino.resourceactions
             actualTable = QueryUtils.getTableFromQueryString(actualDatabase, query);
         }
         for (CrudProperty property : properties) {
-            property.init(persistence.getModel(), persistence.getConfiguration());
+            property.init(persistence.getModel(), persistence.getConfiguration().getProperties());
         }
         if(actualTable != null) {
             for (SelectionProviderReference ref : selectionProviders) {
