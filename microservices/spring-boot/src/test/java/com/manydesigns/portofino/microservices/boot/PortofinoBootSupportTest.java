a2c8b1c540eedacd2da9a5a196a54aee6f4d1916
Alessio Stalla <alessiostalla@gmail.com>
2022-10-03 18:05:37 +0200

    #577 Boot-native deployment without dispatcher: more testing

--- microservices/spring-boot/src/test/java/com/manydesigns/portofino/microservices/boot/PortofinoBootSupportTest.java
+++ microservices/spring-boot/src/test/java/com/manydesigns/portofino/microservices/boot/PortofinoBootSupportTest.java
@@ -1,13 +1,22 @@
 package com.manydesigns.portofino.microservices.boot;
 
 import com.manydesigns.portofino.modules.DatabaseModule;
+import com.manydesigns.portofino.persistence.Persistence;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest(classes = { PortofinoSupport.class, DatabaseModule.class })
 class PortofinoBootSupportTest {
 
 	@Test
-	void contextLoads() {}
+	void contextLoads() {
+		assertEquals("Persistence status", Persistence.Status.STARTED, persistence.status.getValue());
+	}
+
+	@Autowired
+	public Persistence persistence;
 
 }
